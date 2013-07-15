cafe9a5158ec370256a4f3da326d8e653be35a9f
carlos silva <cefs1984@gmail.com>
2013-07-15 15:08:57 -0300

    Autorizando tipo de usuários

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,8 +1,17 @@
+# encoding: utf-8
 class Ability
   include CanCan::Ability
   def initialize(user)
+    user ||= User.new
+
     if user.role == "admin"
-      can [:approved], Advertising
+      can [:read, :update, :create, :destroy, :approved], :all
+    end
+
+    if user.persisted? && user.role == "visitor"
+      can [:read, :update, :create, :destroy], :all
+    else
+      can [:read], :all
     end
   end
 end
