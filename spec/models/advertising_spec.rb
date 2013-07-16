18a11a5dd1afdaf92a54995ac3bf98ef0bb41cd1
carlos silva <cefs1984@gmail.com>
2013-07-15 21:31:06 -0300

    Refatorando testes de model.

--- spec/models/advertising_spec.rb
+++ spec/models/advertising_spec.rb
@@ -1,5 +1,13 @@
 require 'spec_helper'
 
 describe Advertising do
-  it { should belong_to(:user) }
+  context "relationships" do
+    it { should belong_to(:user) }
+  end
+
+  context "validations" do
+    it { should validate_presence_of :description }
+    it { should validate_presence_of :status }
+    it { should validate_presence_of :title }
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,18 @@
 require 'spec_helper'
 
 describe User do
-  it { should have_many(:anuncios).dependent(:restrict) }
+  context "default values" do
+    it "uses visitor as default for status" do
+      expect(subject.role).to eq('visitor')
+    end
+  end
+
+  context "relationships" do
+    it { should have_many(:anuncios).dependent(:restrict) }
+  end
+
+  context "validations" do
+    it { should validate_presence_of :email }
+    it { should validate_presence_of :password }
+  end
 end
