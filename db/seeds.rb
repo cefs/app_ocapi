92b563af688b0093ec128614a6b91da0cef9fdd9
carlos silva <cefs1984@gmail.com>
2013-07-15 22:36:20 -0300

    Corrigindo interpolação no arquivo seed.

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,11 +6,11 @@ visitante = User.new email: 'visitante@appocapi.com', password: '12345678', pass
 visitante.save
 
 7.times.each do |i|
-  Advertising.create title: "Meu carro #{i} km", description: 'Meu carro esta em otimo estado desde 199#{i}!', user: admin, status: Status::APPROVED
+  Advertising.create title: "Meu carro #{i} km", description: "Meu carro esta em otimo estado desde 199#{i}!", user: admin, status: Status::APPROVED
 end
 
 3.times.each do |i|
-  Advertising.create title: 'Minha torradeira 198#{i}', description: 'minha torradeira de 198#{i} tem #{i} utilizadade(s)', user: visitante, status: Status::PENDING
+  Advertising.create title: "Minha torradeira 198#{i}", description: "minha torradeira de 198#{i} tem #{i} utilizadade(s)", user: visitante, status: Status::PENDING
 end
 
 
