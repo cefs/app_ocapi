0b4f313ee822a3c150ce8138d0d9c9563d80c795
carlos silva <cefs1984@gmail.com>
2013-07-15 22:28:37 -0300

    Seed com exemplo de usuários e anúncios.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,16 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+admin = User.new email: 'admin@appocapi.com', password: '12345678', password_confirmation: '12345678'
+admin.role = 'admin'
+admin.save
+
+visitante = User.new email: 'visitante@appocapi.com', password: '12345678', password_confirmation: '12345678'
+visitante.save
+
+7.times.each do |i|
+  Advertising.create title: "Meu carro #{i} km", description: 'Meu carro esta em otimo estado desde 199#{i}!', user: admin, status: Status::APPROVED
+end
+
+3.times.each do |i|
+  Advertising.create title: 'Minha torradeira 198#{i}', description: 'minha torradeira de 198#{i} tem #{i} utilizadade(s)', user: visitante, status: Status::PENDING
+end
+
+
